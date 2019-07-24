9e44dee4ae8870d130b119d63a9c899e8c8a14b7
Lucas Leifheit <lucaslnardon@gmail.com>
2019-07-23 23:13:41 -0300

    New Music Added

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,59 @@ function play1 (){
         document.getElementById('play1').style.display = 'none';
     }
 }
+
+function play2 (){
+    if (isPlaying){
+        document.getElementById('player2').pause();
+        isPlaying = false;
+        document.getElementById('pause2').style.display = 'none';
+        document.getElementById('play2').style.display = 'flex';
+    } else {
+        document.getElementById('player2').play();
+        isPlaying = true;
+        document.getElementById('pause2').style.display = 'flex';
+        document.getElementById('play2').style.display = 'none';
+    }
+}
+
+function play3 (){
+    if (isPlaying){
+        document.getElementById('player3').pause();
+        isPlaying = false;
+        document.getElementById('pause3').style.display = 'none';
+        document.getElementById('play3').style.display = 'flex';
+    } else {
+        document.getElementById('player3').play();
+        isPlaying = true;
+        document.getElementById('pause3').style.display = 'flex';
+        document.getElementById('play3').style.display = 'none';
+    }
+}
+
+function play4 (){
+    if (isPlaying){
+        document.getElementById('player4').pause();
+        isPlaying = false;
+        document.getElementById('pause4').style.display = 'none';
+        document.getElementById('play4').style.display = 'flex';
+    } else {
+        document.getElementById('player4').play();
+        isPlaying = true;
+        document.getElementById('pause4').style.display = 'flex';
+        document.getElementById('play4').style.display = 'none';
+    }
+}
+
+function play5 (){
+    if (isPlaying){
+        document.getElementById('player5').pause();
+        isPlaying = false;
+        document.getElementById('pause5').style.display = 'none';
+        document.getElementById('play5').style.display = 'flex';
+    } else {
+        document.getElementById('player5').play();
+        isPlaying = true;
+        document.getElementById('pause5').style.display = 'flex';
+        document.getElementById('play5').style.display = 'none';
+    }
+}
